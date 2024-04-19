357c8e653839671baeb94cd97800d85f17cd9a09
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-04-19 13:05:19 +0200

    Add/Hide git command

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,7 +4,7 @@ return {
 
         config = function()
             vim.keymap.set({ "n", "v" }, "<leader>gb", ":G blame<CR>", {})
-            vim.keymap.set({ "n", "v" }, "<leader>gc", ":G commit<CR>", {})
+            --vim.keymap.set({ "n", "v" }, "<leader>gc", ":G commit<CR>", {})
             vim.keymap.set({ "n", "v" }, "<leader>gl", ":G log<CR>", {})
         end
     },
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -57,6 +57,7 @@ return {
 
             vim.keymap.set("n", "<leader>tc", builtin.git_commits, {})
             vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
+            vim.keymap.set("n", "<leader>bl", builtin.git_branches, {})
             -- vim.keymap.set("n", "<C-p>", builtin.git_files, {})
         end,
     },
