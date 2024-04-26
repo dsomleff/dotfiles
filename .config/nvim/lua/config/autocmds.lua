bea37b46d30cbc7980516f26c7c7e349f340cf6b
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-04-26 09:18:22 +0200

    Fix status line branch display

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -42,13 +42,12 @@ function my_statusline()
     local branch = vim.fn.FugitiveHead()
 
     if branch and #branch > 0 then
-        branch = " " .. branch
+        --branch = " " .. branch .."  "
+        branch = " " .. branch .. " | "
     end
 
     return "%<"
         .. branch
-        .. " | "
-        --.. '  '
         .. "%f"
         .. " "
         .. "%m"
