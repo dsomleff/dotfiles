0774df350091a5dba4e83d4697f1d8dae352c9fe
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-04-26 08:56:54 +0200

    Rewrite statusline

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -25,30 +25,7 @@ autocmd("BufEnter", {
     command = "if !argc() | Explore | endif",
 })
 
--- grab the name of the current git branch
-local function branch_name()
-    local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
-    if branch ~= "" then
-        return branch .. " | "
-    else
-        return ""
-    end
-end
-
-local function file_name()
-    local root_path = vim.fn.getcwd()
-    local root_dir = root_path:match("[^/]+$")
-    local home_path = vim.fn.expand("%:~")
-    local overlap, _ = home_path:find(root_dir)
-    if home_path == "" then
-        return root_path:gsub("/Users/[^/]+", "~")
-    elseif overlap then
-        return home_path:sub(overlap)
-    else
-        return home_path
-    end
-end
-
+-------------------STATUSLINE----------------------------
 local function progress()
     if vim.fn.line(".") == 1 then
         return "top"
@@ -61,29 +38,31 @@ local function progress()
     end
 end
 
-autocmd({ "FileType", "BufEnter", "FocusGained", "CursorHold", "CursorHoldI", "FileChangedShellPost" }, {
-    callback = function()
-        vim.b.branch_name = branch_name()
-        vim.b.file_name = file_name()
-    end
-})
+function my_statusline()
+    local branch = vim.fn.FugitiveHead()
 
-function Status_Line()
-    return " "
-        .. "%<"
-        .. vim.b.branch_name
-        .. vim.b.file_name
+    if branch and #branch > 0 then
+        branch = " " .. branch
+    end
+
+    return "%<"
+        .. branch
+        -- " | "
+        .. '  '
+        .. "%f"
         .. " "
-        .. "%h"
         .. "%m"
         .. "%="
         .. "%y"
+        .. " "
+        .. "%l:%c"
         .. " "
         .. progress()
         .. " "
 end
 
-vim.opt.statusline = "%{%v:lua.Status_Line()%}"
+vim.opt.statusline = "%{%v:lua.my_statusline()%}"
+-------------------END STATUSLINE----------------------------
 
 -- highlight on yank
 autocmd('TextYankPost', {
