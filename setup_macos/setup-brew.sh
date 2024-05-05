c87ce9937377cd373f3f2d95a1a95bbc435c5c2e
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-05-05 11:13:20 +0200

    Create a setup-brew script/ CLI part

---
+++ setup_macos/setup-brew.sh
@@ -0,0 +1,11 @@
+# Upgrade brew
+brew upgrade
+
+# Install CLI tools
+brew install zsh
+brew install fzf
+brew install zoxide
+brew install git
+brew install neovim
+brew install tmux
+brew install stow
