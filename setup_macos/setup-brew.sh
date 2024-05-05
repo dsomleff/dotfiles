c4d1c84790db8b8e650541c4f803dbf9257a8274
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-05-05 11:33:28 +0200

    Add dev tools brew

--- setup_macos/setup-brew.sh
+++ setup_macos/setup-brew.sh
@@ -9,3 +9,18 @@ brew install git
 brew install neovim
 brew install tmux
 brew install stow
+brew install alacritty
+
+# Install dev tools
+brew install --cask docker
+brew install insomnia
+brew install raycast
+brew install --cask arc
+
+brew tap homebrew/cask-fonts
+brew install --cask font-jetbrains-mono
+
+brew install --cask webstorm
+
+# Other tools
+brew install --cask rectangle
