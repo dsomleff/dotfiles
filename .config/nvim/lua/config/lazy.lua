ac6ca1d505b8bc1fb82327804ef87bde7c629517
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-05-07 20:35:31 +0200

    Add checker for plugins in lazy.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,6 +15,10 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = "plugins",
+    checker = {
+        enabled = true,
+        notify = false
+    },
     change_detection = { notify = false }
 })
 
