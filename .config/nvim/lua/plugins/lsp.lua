9ff114a9fe68ab5ba3453c622c2ed8983d4d15e0
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-05-09 17:21:21 +0200

    tmux and alacritty conf upadate

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,105 +1,118 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/nvim-cmp',
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        "j-hui/fidget.nvim",
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+	},
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+	config = function()
+		local cmp = require("cmp")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-        require("fidget").setup({})
-        require("mason").setup({})
-        require("mason-lspconfig").setup {
-            ensure_installed = {
-                "lua_ls",
-                "tsserver",
-                "cssls",
-                "eslint",
-                "html",
-                "jsonls",
-                "prosemd_lsp", -- markdown
-                "taplo",       -- toml
-                "volar",
-                "golangci_lint_ls"
-            },
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup {
-                        -- capabilities = capabilities
-                    }
-                end,
+		require("fidget").setup({})
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"tsserver",
+				"cssls",
+				"html",
+				"jsonls",
+				"eslint",
+				"prosemd_lsp", -- markdown
+				"taplo", -- toml
+				"volar",
+				"golangci_lint_ls",
+			},
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						-- capabilities = capabilities
+					})
+				end,
 
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" }
-                                }
-                            }
-                        }
-                    }
-                end,
-            },
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    --["<Tab>"] =  cmp.mapping.confirm({ select = true }),
+				["volar"] = function()
+					require("lspconfig").volar.setup({
+						capabilities = capabilities,
+						filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+						init_options = {
+							vue = {
+								hybridMode = false,
+							},
+						},
+					})
+				end,
+			},
 
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' }, -- For luasnip users.
-                }, {
-                    { name = 'buffer' },
-                })
-            }),
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+					--["<Tab>"] =  cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+				}, {
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			}),
 
-            vim.diagnostic.config({
-                update_in_insert = true,
-                float = {
-                    focusable = false,
-                    style     = "minimal",
-                    border    = "rounded",
-                    source    = "always",
-                    header    = "",
-                    prefix    = "",
-                }
-            })
-        }
+			vim.diagnostic.config({
+				update_in_insert = true,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+			}),
+		})
 
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-        vim.keymap.set({ "n", "v" }, "<F2>", vim.lsp.buf.rename, {})
-    end
+		vim.keymap.set({ "n", "v" }, "<F2>", vim.lsp.buf.rename, {})
+	end,
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,45 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/nvim-treesitter",
 
-    build = ":TSUpdate",
+	build = ":TSUpdate",
 
-    config = function()
-        local config = require("nvim-treesitter.configs")
+	config = function()
+		local config = require("nvim-treesitter.configs")
 
-        config.setup({
-            autotag = {
-                enable = true,
-            },
+		config.setup({
+			autotag = {
+				enable = true,
+			},
 
-            ensure_installed = {
-                "vimdoc",
-                "javascript",
-                "typescript",
-                "lua",
-                "go",
-                "jsdoc",
-                "json",
-                "yaml",
-                "html",
-                "css",
-                "markdown",
-                "bash",
-            },
+			ensure_installed = { "vimdoc", "javascript", "typescript", "lua", "go", "jsdoc" },
 
-            sync_install = false,
+			sync_install = false,
 
-            auto_install = true,
+			auto_install = true,
 
-            indent = {
-                enable = true
-            },
+			indent = {
+				enable = true,
+			},
 
-            highlight = {
-                enable = true,
+			highlight = {
+				enable = true,
 
-
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-        })
-    end,
+				additional_vim_regex_highlighting = { "markdown" },
+			},
+		})
+	end,
 }
