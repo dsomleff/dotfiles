4576de3236b0c041805c4d3676c7679297a0f39a
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-05-16 09:50:45 +0200

    Add history for search and command + font settings

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -76,3 +76,6 @@ vim.cmd("runtime macros/matchit.vim")
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
+
+-- Remember history of the search / command-line
+vim.opt.shada = { "'10", "<0", "s10", "h" }
