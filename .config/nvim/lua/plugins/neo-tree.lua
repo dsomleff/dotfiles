a4a4f5ca5af55f4079d42c6186e56a23ab32adc2
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-05-28 17:31:39 +0200

    Lint neo-tree conf file

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,13 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons" ,
-            "MunifTanjim/nui.nvim",
-        },
-        config = function()
-            vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
-        end
-    }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+		end,
+	},
 }
