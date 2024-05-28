3198975c4355c1a9ea979535fdef62d8ad18c7c4
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-05-28 11:18:39 +0200

    Change theme from rose pine to kanagawa - fugitive colors was fixed - still no cursor highlight

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,26 +1,30 @@
 return {
-    {
-        "rose-pine/neovim",
-        lazy = false,
-        name = "rose-pine",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("rose-pine")
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		name = "kanagawa",
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({
+				dimInactive = true,
+				transparent = true,
+			})
+			vim.cmd.colorscheme("kanagawa-dragon")
 
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        end,
-    },
-    {
-        "eandrju/cellular-automaton.nvim",
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		end,
+	},
+	{
+		"eandrju/cellular-automaton.nvim",
 
-        vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>"),
-        vim.keymap.set("n", "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>"),
-        vim.keymap.set("n", "<leader>mtx", "<cmd>CellularAutomaton scramble<CR>")
-    },
-    {
-        "folke/zen-mode.nvim",
+		vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>"),
+		vim.keymap.set("n", "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>"),
+		vim.keymap.set("n", "<leader>mtx", "<cmd>CellularAutomaton scramble<CR>"),
+	},
+	{
+		"folke/zen-mode.nvim",
 
-        vim.keymap.set("n", "<leader>zz", ":ZenMode<CR>"),
-    }
+		vim.keymap.set("n", "<leader>zz", ":ZenMode<CR>"),
+	},
 }
