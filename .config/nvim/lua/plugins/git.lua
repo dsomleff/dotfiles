d68dad33cf9b6099d44e16b2690db4d9eeffce76
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-05-29 18:48:54 +0200

    Add next/prev hunk keymap

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -59,31 +59,8 @@ return {
 				vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {}),
 				vim.keymap.set({ "n", "v" }, "<leader>tb", ":Gitsigns blame_line <CR>", {}),
 				vim.keymap.set({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk <CR>", {}),
-
-				vim.keymap.set("n", "]c", function()
-					if vim.wo.diff then
-						return "]c"
-					end
-					vim.schedule(function()
-						gs.prev_hunk()
-					end)
-					return "<Ignore>"
-				end, { expr = true }),
-
-				vim.keymap.set("n", "[c", function()
-					if vim.wo.diff then
-						return "[c"
-					end
-					vim.schedule(function()
-						gs.prev_hunk()
-					end)
-					return "<Ignore>"
-				end, { expr = true }),
-
-				--vim.keymap.set("n", "<leader>tc", builtin.git_commits, {})
-				--vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
-				--vim.keymap.set("n", "<leader>bl", builtin.git_branches, {})
-				--vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+				vim.keymap.set({ "n" }, "]h", ":Gitsigns next_hunk <CR>", {}),
+				vim.keymap.set({ "n" }, "[h", ":Gitsigns prev_hunk <CR>", {}),
 			})
 		end,
 	},
