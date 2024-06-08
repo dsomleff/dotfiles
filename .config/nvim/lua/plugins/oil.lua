35f68c845ea2d449969feab5f6fb4c0aa6ba7054
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-06-08 16:20:08 +0200

    Configure Oil
    - rollback telescope keymap

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -6,8 +6,22 @@ return {
 		config = function()
 			require("oil").setup({
 				default_file_explorer = true,
+				delete_to_trash = true,
+				use_default_keymaps = false,
 				view_options = {
 					show_hidden = true,
+				},
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					["<C-c>"] = "actions.close",
+					["<C-l>"] = "actions.refresh",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["`"] = "actions.cd",
+					["~"] = "actions.tcd",
+					["gs"] = "actions.change_sort",
+					["g."] = "actions.toggle_hidden",
 				},
 
 				vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Open parent directory" }),
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -51,7 +51,7 @@ return {
 
 			local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "[p", builtin.find_files, {})
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
