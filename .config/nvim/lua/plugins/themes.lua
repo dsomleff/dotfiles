96e318dc83f991457b150adc85572d0f737e4857
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-06-20 17:03:52 +0200

    Change themes keymaps

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -16,7 +16,7 @@ return {
 
 		vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>"),
 		vim.keymap.set("n", "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>"),
-		vim.keymap.set("n", "<leader>mtx", "<cmd>CellularAutomaton scramble<CR>"),
+		vim.keymap.set("n", "<leader>mx", "<cmd>CellularAutomaton scramble<CR>"),
 	},
 	{
 		"folke/zen-mode.nvim",
