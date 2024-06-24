b5a00648300ab3b430c418e1bab3314dcf6b876b
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-06-24 15:38:37 +0200

    Update indent plugin (maybe I need enable it) Lazy update 24.06.2024

--- .config/nvim/lua/fancy_stuff/indent.lua
+++ .config/nvim/lua/fancy_stuff/indent.lua
@@ -1,24 +1,16 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-
-    config = function()
-        require("ibl").setup({
-            scope = {
-                char = "|",
-                show_start = false,
-                enabled = false,
-            },
-            indent = {
-                char = "|",
-                --tab_char = { "a", "b", "c" },
-                --highlight = { "Function", "Label" },
-                smart_indent_cap = false,
-
-            },
-            whitespace = {
-                highlight = { "Function", "Label" },
-                remove_blankline_trail = true,
-            },
-        })
-    end,
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		event = "VeryLazy",
+		opts = {
+			indent = {
+				char = "│",
+			},
+			scope = {
+				show_start = false,
+				show_end = false,
+			},
+		},
+	},
 }
