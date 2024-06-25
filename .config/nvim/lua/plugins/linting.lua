12420653f10e2931e9ef8b008caa03f95fcb67db
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-06-25 14:53:48 +0200

    Rollback linting settings
    - It has no impact on performance

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -27,7 +27,7 @@ return {
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "InsertLeave" }, { -- BufEnter, BufWritePost
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
 				if has_eslint_config() then
