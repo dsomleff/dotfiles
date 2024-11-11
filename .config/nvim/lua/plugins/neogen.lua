c09e0f07c6e131303bc0d83316e08c078ef853bc
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-11-11 18:29:26 +0100

    Make [void delete to] motion work

--- .config/nvim/lua/plugins/neogen.lua
+++ .config/nvim/lua/plugins/neogen.lua
@@ -1,26 +1,26 @@
 return {
-    "danymat/neogen",
+	"danymat/neogen",
 
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "L3MON4D3/LuaSnip"
-    },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"L3MON4D3/LuaSnip",
+	},
 
-    config = function()
-        local neogen = require("neogen")
+	config = function()
+		local neogen = require("neogen")
 
-        neogen.setup({
-            snippet_engine = "luasnip"
-        })
+		neogen.setup({
+			snippet_engine = "luasnip",
+		})
 
-        vim.keymap.set("n", "<leader>df", function()
-            neogen.generate({ type = "func" })
-        end)
-
-        vim.keymap.set("n", "<leader>dt", function()
-            neogen.generate({ type = "type" })
-        end)
-    end,
-    -- Uncomment next line if you want to follow only stable versions
-    -- version = "*"
+		-- vim.keymap.set("n", "<leader>df", function()
+		--     neogen.generate({ type = "func" })
+		-- end)
+		--
+		-- vim.keymap.set("n", "<leader>dt", function()
+		--     neogen.generate({ type = "type" })
+		-- end)
+	end,
+	-- Uncomment next line if you want to follow only stable versions
+	-- version = "*"
 }
