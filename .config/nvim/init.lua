41de05844ed5d8eba2a39ccdca94c59955e9d5c2
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-12-20 12:41:39 +0100

    Add error handling for init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,18 @@
 require("config.options")
-
 require("config.keymap")
-
 require("config.lazy")
-
 require("config.autocmds")
+
+local modules = {
+	"config.options",
+	"config.keymap",
+	"config.lazy",
+	"config.autocmds",
+}
+
+for _, module in ipairs(modules) do
+	local ok, err = pcall(require, module)
+	if not ok then
+		vim.notify("Failed to load " .. module .. "\n" .. err, vim.log.levels.ERROR)
+	end
+end
