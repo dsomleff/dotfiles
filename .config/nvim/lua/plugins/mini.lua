2c13b261619718dfbea1c4deb97a82b509ea98e6
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-02-06 20:19:02 +0100

    Reformat mini.lua

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,35 +1,36 @@
 return {
-	{
-		"echasnovski/mini.ai",
-		version = false,
+    {
+        "echasnovski/mini.ai",
+        version = false,
 
-		config = function()
-			require("mini.ai").setup({
-				-- around/next quotes = "aq"
-				--around_last = "al",
-				--inside_last = "il",
-			})
-		end,
-	},
-	{
-		"echasnovski/mini.surround",
-		version = false,
+        config = function()
+            require("mini.ai").setup({
+                -- around/next quotes = "aq"
+                -- around/next brackets = "ab"
+                --around_last = "al",
+                --inside_last = "il",
+            })
+        end,
+    },
+    {
+        "echasnovski/mini.surround",
+        version = false,
 
-		config = function()
-			require("mini.surround").setup({
-				-- `saiw)` - add (`sa`) for inner word (`iw`) parenthesis (`)`).
-				-- delete = 'sd', -- Delete surrounding
-				-- sr'" -- Replace surrounding
-				-- srt -- Replace surrounding tags
-			})
-		end,
-	},
-	{
-		"echasnovski/mini.pairs",
-		version = false,
+        config = function()
+            require("mini.surround").setup({
+                -- `saiw)` - add (`sa`) for inner word (`iw`) parenthesis (`)`).
+                -- delete = 'sd', -- Delete surrounding
+                -- sr'" -- Replace surrounding
+                -- srt -- Replace surrounding tags
+            })
+        end,
+    },
+    {
+        "echasnovski/mini.pairs",
+        version = false,
 
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
+        config = function()
+            require("mini.pairs").setup()
+        end,
+    },
 }
