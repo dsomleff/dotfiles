4c1c499e10323e61322fc6149ff3af98c133cb59
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-02-06 18:15:16 +0100

    Mini package
    - Replace surround and autopairs plugins

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "echasnovski/mini.ai",
+        version = false,
+
+        config = function()
+            require("mini.ai").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.surround",
+        version = false,
+
+        config = function()
+            require("mini.surround").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.pairs",
+        version = false,
+
+        config = function()
+            require("mini.pairs").setup()
+        end,
+    },
+}
