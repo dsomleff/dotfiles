6d5a925a7cb2c44029d53dbe87706c7389c4f553
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-02-13 14:50:57 +0100

    Change lualine theme = "base16"

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,51 +1,51 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "nord",
-				section_separators = { left = "", right = "" },
-				component_separators = { left = "", right = "" },
-				always_divide_middle = true,
-				globalstatus = false,
-				-- refresh = {
-				-- 	statusline = 1000,
-				-- 	tabline = 1000,
-				-- 	winbar = 1000,
-				-- },
-			},
-			sections = {
-				lualine_a = {
-					{ "branch", icon = "" },
-				},
-				lualine_b = {
-					{
-						"filename",
-						file_status = true,
-						path = 1,
-					},
-				},
-				lualine_c = {
-					"diff",
-					{ "diagnostics", sources = { "nvim_diagnostic" } },
-				},
-				lualine_x = { "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = {},
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "base16",
+                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
+                always_divide_middle = true,
+                globalstatus = false,
+                -- refresh = {
+                -- 	statusline = 1000,
+                -- 	tabline = 1000,
+                -- 	winbar = 1000,
+                -- },
+            },
+            sections = {
+                lualine_a = {
+                    { "branch", icon = "" },
+                },
+                lualine_b = {
+                    {
+                        "filename",
+                        file_status = true,
+                        path = 1,
+                    },
+                },
+                lualine_c = {
+                    "diff",
+                    { "diagnostics", sources = { "nvim_diagnostic" } },
+                },
+                lualine_x = { "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { "filename" },
+                lualine_x = { "location" },
+                lualine_y = {},
+                lualine_z = {},
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {},
+        })
+    end,
 }
