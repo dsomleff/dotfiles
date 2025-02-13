d205103a793d8e1112c9877b1d69baaba1cf48c0
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-02-13 15:11:33 +0100

    Rollback to nord, base-16 to many hustle

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require("lualine").setup({
             options = {
-                theme = "base16",
+                theme = "nord",
                 section_separators = { left = "", right = "" },
                 component_separators = { left = "", right = "" },
                 always_divide_middle = true,
