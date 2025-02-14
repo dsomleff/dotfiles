ce7925f0b32c9ba20d596da30fd8f7570e806677
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-02-14 11:07:54 +0100

    Test lua lsp and formatter

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,29 +1,29 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettierd", "js_beautify", stop_after_first = true },
-				typescript = {},
-				-- javascriptreact = { "prettier" },
-				-- typescriptreact = { "prettier" },
-				vue = { "prettier" },
-				css = { "prettierd", "css_beautify", stop_after_first = true },
-				html = { "prettierd", "html_beautify", stop_after_first = true },
-				-- json = { "prettier" },
-				-- yaml = { "prettier" },
-				markdown = { "prettier" },
-				-- lua = { "stylua" },
-			},
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettierd", "js_beautify", stop_after_first = true },
+                typescript = {},
+                -- javascriptreact = { "prettier" },
+                -- typescriptreact = { "prettier" },
+                vue = { "prettier" },
+                css = { "prettierd", "css_beautify", stop_after_first = true },
+                html = { "prettierd", "html_beautify", stop_after_first = true },
+                -- json = { "prettier" },
+                -- yaml = { "prettier" },
+                markdown = { "prettier" },
+                lua = { "stylua" },
+            },
 
-			format_on_save = {
-				lsp_format = "fallback",
-				async = false,
-				timeout_ms = 500,
-			},
-		})
-	end,
+            format_on_save = {
+                lsp_format = "fallback",
+                async = false,
+                timeout_ms = 500,
+            },
+        })
+    end,
 }
