1a80312d0c6881b395be253c8add8a07fe5a77c5
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-02-14 14:47:58 +0100

    Play around formatting and lsp settings

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,112 +1,114 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "saghen/blink.cmp",
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "rafamadriz/friendly-snippets", -- for snippets
-        "j-hui/fidget.nvim",
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"saghen/blink.cmp",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"rafamadriz/friendly-snippets", -- for snippets
+		"j-hui/fidget.nvim",
+	},
 
-    config = function()
-        -- Get LSP capabilities from blink.cmp
-        local capabilities = require("blink.cmp").get_lsp_capabilities()
+	config = function()
+		-- Get LSP capabilities from blink.cmp
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-        require("fidget").setup({})
-        require("mason").setup({})
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "ts_ls",
-                "css_variables",
-                "html",
-                "jsonls",
-                "eslint",
-                "harper_ls", -- markdown
-                "taplo",     -- toml
-                "volar",
-                "yamlls",
-            },
-            handlers = {
-                function(server_name)
-                    local server_config = { capabilities = capabilities }
+		require("fidget").setup({})
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"ts_ls",
+				"css_variables",
+				"html",
+				"jsonls",
+				"eslint",
+				"harper_ls", -- markdown
+				"taplo", -- toml
+				"volar",
+				"yamlls",
+			},
+			handlers = {
+				function(server_name)
+					local server_config = { capabilities = capabilities }
 
-                    if server_name == "volar" then
-                        server_config.filetypes = { "vue", "typescript", "javascript" }
-                    end
+					if server_name == "volar" then
+						server_config.filetypes = { "vue", "typescript", "javascript" }
+					end
 
-                    require("lspconfig")[server_name].setup(server_config)
-                end,
+					require("lspconfig")[server_name].setup(server_config)
+				end,
 
-                ["lua_ls"] = function()
-                    require("lspconfig").lua_ls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                            },
-                        },
-                    })
-                end,
+				["lua_ls"] = function()
+					require("lspconfig").lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
 
-                ["volar"] = function()
-                    require("lspconfig").volar.setup({
-                        capabilities = capabilities,
-                        filetypes = { "vue", "javascript", "typescript" },
-                        init_options = {
-                            vue = {
-                                hybridMode = false,
-                            },
-                        },
-                        on_attach = function(client)
-                            client.server_capabilities.documentFormattingProvider = false
-                            client.server_capabilities.documentRangeFormattingProvider = false
-                        end,
-                    })
-                end,
-                ["harper_ls"] = function()
-                    require("lspconfig").harper_ls.setup({
-                        filetypes = { "markdown" },
-                        on_attach = function(client)
-                            client.server_capabilities.documentFormattingProvider = true
-                        end,
-                    })
-                end,
-                ["yamlls"] = function()
-                    require("lspconfig").harper_ls.setup({
-                        filetypes = { "yaml" },
-                        on_attach = function(client)
-                            client.server_capabilities.documentFormattingProvider = true
-                        end,
-                    })
-                end,
-            },
-        })
+				["volar"] = function()
+					require("lspconfig").volar.setup({
+						capabilities = capabilities,
+						filetypes = { "vue", "javascript", "typescript" },
+						init_options = {
+							vue = {
+								hybridMode = false,
+							},
+						},
+						on_attach = function(client)
+							client.server_capabilities.documentFormattingProvider = false
+							client.server_capabilities.documentRangeFormattingProvider = false
+						end,
+					})
+				end,
+				["harper_ls"] = function()
+					require("lspconfig").harper_ls.setup({
+						capabilities = capabilities,
+						filetypes = { "markdown" },
+						on_attach = function(client)
+							client.server_capabilities.documentFormattingProvider = true
+						end,
+					})
+				end,
+				["yamlls"] = function()
+					require("lspconfig").yamlls.setup({
+						capabilities = capabilities,
+						filetypes = { "yaml" },
+						on_attach = function(client)
+							client.server_capabilities.documentFormattingProvider = true
+						end,
+					})
+				end,
+			},
+		})
 
-        -- Diagnostic configuration
-        vim.diagnostic.config({
-            update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-            virtual_text = {
-                spacing = 4,
-                prefix = "●",
-            },
-        })
+		-- Diagnostic configuration
+		vim.diagnostic.config({
+			update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+			virtual_text = {
+				spacing = 4,
+				prefix = "●",
+			},
+		})
 
-        -- LSP Keymappings
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        vim.keymap.set({ "n", "v" }, "<F2>", vim.lsp.buf.rename, {})
-    end,
+		-- LSP Keymappings
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		vim.keymap.set({ "n", "v" }, "<F2>", vim.lsp.buf.rename, {})
+	end,
 }
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,37 +1,37 @@
 return {
-    {
-        "echasnovski/mini.ai",
-        version = false,
+	{
+		"echasnovski/mini.ai",
+		version = false,
 
-        config = function()
-            require("mini.ai").setup({
-                -- around/next quotes = "aq"
-                -- around/next brackets = "ab"
-                -- around/next arguments = "aa"
-                --around_last = "al",
-                --inside_last = "il",
-            })
-        end,
-    },
-    {
-        "echasnovski/mini.surround",
-        version = false,
+		config = function()
+			require("mini.ai").setup({
+				-- around/next quotes = "aq"
+				-- around/next brackets = "ab"
+				-- around/next arguments = "aa"
+				--around_last = "al",
+				--inside_last = "il",
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		version = false,
 
-        config = function()
-            require("mini.surround").setup({
-                -- `saiw)` - add (`sa`) for inner word (`iw`) parenthesis (`)`).
-                -- delete = 'sd', -- Delete surrounding
-                -- sr'" -- Replace surrounding
-                -- srt -- Replace surrounding tags
-            })
-        end,
-    },
-    {
-        "echasnovski/mini.pairs",
-        version = false,
+		config = function()
+			require("mini.surround").setup({
+				-- `saiw)` - add (`sa`) for inner word (`iw`) parenthesis (`)`).
+				-- delete = 'sd', -- Delete surrounding
+				-- sr'" -- Replace surrounding
+				-- srt -- Replace surrounding tags
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		version = false,
 
-        config = function()
-            require("mini.pairs").setup()
-        end,
-    },
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
 }
