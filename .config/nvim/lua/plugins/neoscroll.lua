f11b0fc3911e6404f2821b4a5e7729f4f9c8e090
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-03-02 16:50:59 +0100

    Config neoscroll

--- .config/nvim/lua/plugins/neoscroll.lua
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -1,15 +1,21 @@
 return {
 	"karb94/neoscroll.nvim",
-	opts = {
-		mappings = {
-			"<C-u>",
-			"<C-d>",
-			"<C-b>",
-			"<C-y>",
-			"<C-e>",
-			"zt",
-			"zz",
-			"zb",
-		},
-	},
+	opts = function()
+		local neoscroll = require("neoscroll")
+
+		local keymap = {
+			["<C-u>"] = function()
+				neoscroll.ctrl_u({ duration = 400, easing = "sine" })
+			end,
+			["<C-d>"] = function()
+				neoscroll.ctrl_d({ duration = 400, easing = "sine" })
+			end,
+		}
+
+		return {
+			mappings = { "<C-u>", "<C-d>" },
+			duration_multiplier = 0.3,
+			keymap = keymap, -- Assign keymap to config
+		}
+	end,
 }
