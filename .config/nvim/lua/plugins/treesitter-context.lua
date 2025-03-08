7a217aa3db94a54e5aab92ea710e40cb93937e40
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-03-08 21:52:04 +0100

    treesitter-context add separate line

--- .config/nvim/lua/plugins/treesitter-context.lua
+++ .config/nvim/lua/plugins/treesitter-context.lua
@@ -1,3 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+        vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "Grey" })
+        vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true, sp = "Grey" })
+    end,
 }
