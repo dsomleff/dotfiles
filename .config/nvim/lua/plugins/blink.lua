fba6ab2ee35698c81487345eaf0846f49628f916
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-03-16 00:14:07 +0100

    Remove unnecessary dependency from blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,6 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = "rafamadriz/friendly-snippets",
+	-- dependencies = "rafamadriz/friendly-snippets",
 	version = "v0.*",
 	opts = {
 		keymap = { preset = "default" },
