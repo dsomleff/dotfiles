c6c1c46049f7bd8cbf3570e1b60cd43b64151753
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-03-31 09:37:49 +0200

    Commented out keymaps for quick fix list and buffers
    - It's now default for nvim 0.11

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -34,10 +34,10 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- quick fix navigation
-vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "[Q", "<cmd>cfirst<CR>zz")
-vim.keymap.set("n", "]Q", "<cmd>clast<CR>zz")
+-- vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "[Q", "<cmd>cfirst<CR>zz")
+-- vim.keymap.set("n", "]Q", "<cmd>clast<CR>zz")
 
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
@@ -47,7 +47,7 @@ vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left>
 
 -- instead of :so
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+	vim.cmd("so")
 end)
 
 -- navigate vim pane
@@ -61,10 +61,10 @@ vim.keymap.set("n", "[a", "gg<S-v>G")
 vim.keymap.set("n", "]a", "gg<S-v>G")
 
 -- Navigating buffers
-vim.keymap.set("n", "[b", ":bprevious<CR>")
-vim.keymap.set("n", "]b", ":bnext<CR>")
-vim.keymap.set("n", "[B", ":bfirst<CR>")
-vim.keymap.set("n", "]B", ":blast<CR>")
+-- vim.keymap.set("n", "[b", ":bprevious<CR>")
+-- vim.keymap.set("n", "]b", ":bnext<CR>")
+-- vim.keymap.set("n", "[B", ":bfirst<CR>")
+-- vim.keymap.set("n", "]B", ":blast<CR>")
 
 -- Tab's
 vim.keymap.set("v", "<", "<gv")
@@ -75,7 +75,7 @@ vim.opt.conceallevel = 2
 
 -- Display LSP hints
 vim.keymap.set("n", "<leader>h", function()
-    vim.lsp.inlay_hint.enable()
+	vim.lsp.inlay_hint.enable()
 end)
 
 -- Open terminal
@@ -96,8 +96,8 @@ vim.api.nvim_set_keymap("n", "<Right>", "<NOP>", { noremap = true })
 
 -- Paste below the current line while keeping the cursor at the correct position
 vim.keymap.set("n", "p", function()
-    local pos = vim.api.nvim_win_get_cursor(0)
-    local row, col = pos[1], pos[2]
-    vim.cmd("put")
-    vim.api.nvim_win_set_cursor(0, { row + 1, col })
+	local pos = vim.api.nvim_win_get_cursor(0)
+	local row, col = pos[1], pos[2]
+	vim.cmd("put")
+	vim.api.nvim_win_set_cursor(0, { row + 1, col })
 end)
