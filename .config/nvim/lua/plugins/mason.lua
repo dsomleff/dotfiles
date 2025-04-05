96d85f3e9b98ef00f789a0ffe342b059b0ae2a2a
Dmytro Somliev <dmytro.somliev@ibm.com>
2025-04-05 23:46:03 +0200

    Add config to mason-tool-installer

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -20,6 +20,9 @@ return {
 		require("mason").setup()
 		require("mason-tool-installer").setup({
 			ensure_installed = ensure_installed,
+			auto_update = true,
+			run_on_start = true,
+			start_delay = 3000,
 		})
 	end,
 }
