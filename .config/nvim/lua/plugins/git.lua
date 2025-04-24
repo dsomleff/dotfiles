d704b20303075b3652489d10c7d52ac918207b87
Dmytro Somliev <somleff@gmail.com>
2025-04-24 14:35:34 +0200

    Update git config

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -7,7 +7,7 @@ return {
 			{ "<leader>gp", ":G pull<CR>", {} },
 			{ "<leader>gP", ":G push<CR>", {} },
 			{ "<leader>gf", ":G fetch<CR>", {} },
-			{ "<leader>gb", ":G blame <CR>", {} }, -- add -C -C -C to :G blame if you want more deep info; add -w to ignore whitespace
+			-- { "<leader>gb", ":G blame <CR>", {} }, -- add -C -C -C to :G blame if you want more deep info; add -w to ignore whitespace
 			{ "<leader>gd", ":Gvdiff<CR>", {} },
 			{ "<leader>gH", ":G log --oneline --decorate --graph --parents --all<CR>", {} },
 			-- { "<leader>gl", ":G log | only<CR>", {} },
@@ -40,10 +40,12 @@ return {
 			-- Key mappings for Gitsigns
 			local map = vim.keymap.set
 			map("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
-			map({ "n", "v" }, "<leader>gB", ":Gitsigns blame_line<CR>", {})
+			map({ "n", "v" }, "<leader>gbb", ":Gitsigns blame_line<CR>", {})
 			map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", {})
 			map("n", "]h", ":Gitsigns next_hunk<CR>", {})
 			map("n", "[h", ":Gitsigns prev_hunk<CR>", {})
+			map("n", "<leader>gb", ":Gitsigns blame<CR>", {})
+			map("n", "<leader>hd", ":Gitsigns diffthis<CR>", {})
 		end,
 	},
 }
