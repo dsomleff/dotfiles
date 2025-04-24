78f5bec9c890e0ef790ade7d64878fddb6bf11dc
Dmytro Somliev <somleff@gmail.com>
2025-04-24 14:17:38 +0200

    Update blink config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -6,7 +6,7 @@ return {
 		"rafamadriz/friendly-snippets",
 	},
 	opts = {
-		keymap = { preset = "default" },
+		keymap = { preset = "default", ["<Tab>"] = { "select_next", "snippet_forward", "fallback" } },
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
