302720c97529036e519982fb52116dd6893305fb
Dmytro Somliev <somleff@gmail.com>
2025-04-28 15:20:59 +0200

    Update blink config and version

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,14 +1,15 @@
 return {
 	"saghen/blink.cmp",
-	version = "v0.*",
+	version = "1.*",
 	event = { "InsertEnter", "CmdlineEnter" },
 	dependencies = {
 		"rafamadriz/friendly-snippets",
 	},
 	opts = {
-		keymap = { preset = "default", ["<Tab>"] = { "select_next", "snippet_forward", "fallback" } },
+		-- to have a Tab as a select_next key
+		-- keymap = { preset = "default", ["<Tab>"] = { "select_next", "snippet_forward", "fallback" } },
+		keymap = { preset = "super-tab" },
 		appearance = {
-			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
 		},
 		sources = {
@@ -32,10 +33,12 @@ return {
 				border = "single",
 			},
 			documentation = {
+				auto_show = true,
 				window = {
 					border = "single",
 				},
 			},
 		},
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 }
