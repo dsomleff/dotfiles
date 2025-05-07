24bf0d04201a247d78f6577ee3464198c1210a31
Dmytro Somliev <somleff@gmail.com>
2025-05-07 18:27:17 +0200

    Fix prettierd

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,10 +3,10 @@ return {
 	event = { "BufReadPost" },
 	opts = {
 		formatters_by_ft = {
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
-			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettierd", "prettier" },
+			typescript = { "prettierd", "prettier" },
+			javascriptreact = { "prettierd", "prettier" },
+			typescriptreact = { "prettierd", "prettier" },
 			vue = { "prettier" },
 			css = { "prettier" },
 			html = { "prettier" },
@@ -15,6 +15,7 @@ return {
 			markdown = { "prettier" },
 			lua = { "stylua" },
 		},
+		format_after_first = true,
 		default_format_opts = {
 			lsp_format = "fallback",
 		},
