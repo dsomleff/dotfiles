30cc8c47bd121c2eae526c5a3de55fd844406cf1
Dmytro Somliev <somleff@gmail.com>
2025-08-18 09:43:16 +0200

    Update fidget config

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -2,6 +2,10 @@ return {
 	"j-hui/fidget.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		require("fidget").setup({})
+		require("fidget").setup({
+			notification = {
+				window = { winblend = 0 },
+			},
+		})
 	end,
 }
