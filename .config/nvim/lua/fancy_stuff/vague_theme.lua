5861f5c73a06b1f2c7662f089cf112457d3f55ed
Dmytro Somliev <somleff@gmail.com>
2025-11-24 09:20:15 +0100

    Change theme into vague (NY version)

--- .config/nvim/lua/fancy_stuff/vague_theme.lua
+++ .config/nvim/lua/fancy_stuff/vague_theme.lua
@@ -1,32 +1,11 @@
 return {
-	"vague-theme/vague.nvim",
+	"metalelf0/black-metal-theme-neovim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("vague").setup({
-			on_highlights = function(hl, c)
-				-- Git diff statusline highlights
-				hl.StatusLineGitDiffAdded = { fg = c.plus }
-				hl.StatusLineGitDiffChanged = { fg = c.keyword }
-				hl.StatusLineGitDiffRemoved = { fg = c.parameter }
-
-				-- LSP diagnostics statusline highlights
-				hl.StatusLineLspError = { fg = c.error, bold = true }
-				hl.StatusLineLspWarn = { fg = c.warning, bold = true }
-				hl.StatusLineLspHint = { fg = c.constant, bold = true }
-				hl.StatusLineLspInfo = { fg = c.keyword, bold = true }
-
-				-- Example: optional background styling if you want later
-				-- hl.StatusLineGit        = { bg = "#080808" }
-				-- hl.StatusLineFile       = { bg = "#080808" }
-				-- hl.StatusLineCursor     = { bg = "#080808" }
-				-- hl.StatusLinePercent    = { bg = "#080808" }
-				-- hl.StatusLineTotalLines = { bg = "#080808" }
-			end,
+		require("black-metal").setup({
+			-- optional configuration here
 		})
-
-		vim.cmd("colorscheme vague")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		require("black-metal").load()
 	end,
 }
--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,39 +1,32 @@
 return {
-	"slugbyte/lackluster.nvim",
+	"vague-theme/vague.nvim",
 	lazy = false,
 	priority = 1000,
-	init = function()
-		local lackluster = require("lackluster")
-		local color = lackluster.color
+	config = function()
+		require("vague").setup({
+			on_highlights = function(hl, c)
+				-- Git diff statusline highlights
+				hl.StatusLineGitDiffAdded = { fg = c.plus }
+				hl.StatusLineGitDiffChanged = { fg = c.keyword }
+				hl.StatusLineGitDiffRemoved = { fg = c.parameter }
 
-		lackluster.setup({
-			tweak_ui = {
-				enable_end_of_buffer = true,
-			},
-			tweak_highlight = {
-				-- Git diff
-				StatusLineGitDiffAdded = { fg = color.green },
-				StatusLineGitDiffChanged = { fg = color.lack },
-				StatusLineGitDiffRemoved = { fg = color.orange },
+				-- LSP diagnostics statusline highlights
+				hl.StatusLineLspError = { fg = c.error, bold = true }
+				hl.StatusLineLspWarn = { fg = c.warning, bold = true }
+				hl.StatusLineLspHint = { fg = c.constant, bold = true }
+				hl.StatusLineLspInfo = { fg = c.keyword, bold = true }
 
-				-- LSP diagnostics
-				StatusLineLspError = { fg = color.red, bold = true },
-				StatusLineLspWarn = { fg = color.orange, bold = true },
-				StatusLineLspHints = { fg = color.luster, bold = true },
-				StatusLineLspInfo = { fg = color.blue, bold = true },
-
-				-- Status line colors (commented in your config originally)
-				-- StatusLineGit = { bg = "#080808" },
-				-- StatusLineFile = { bg = "#080808" },
-				-- StatusLineCursor = { bg = "#080808" },
-				-- StatusLinePercent = { bg = "#080808" },
-				-- StatusLineTotalLines = { bg = "#080808" },
-			},
+				-- Example: optional background styling if you want later
+				-- hl.StatusLineGit        = { bg = "#080808" }
+				-- hl.StatusLineFile       = { bg = "#080808" }
+				-- hl.StatusLineCursor     = { bg = "#080808" }
+				-- hl.StatusLinePercent    = { bg = "#080808" }
+				-- hl.StatusLineTotalLines = { bg = "#080808" }
+			end,
 		})
 
-		vim.cmd.colorscheme("lackluster-hack")
+		vim.cmd("colorscheme vague")
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "FloatBorder", { fg = color.gray9, bg = "#101010" })
 	end,
 }
