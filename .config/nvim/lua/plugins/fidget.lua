041e513ce3b64fd39821bb333e58f6085b2e8b19
Dmytro Somliev <somleff@gmail.com>
2025-12-09 18:58:52 +0100

    Rewrite fidget config

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -1,11 +1,9 @@
 return {
 	"j-hui/fidget.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("fidget").setup({
-			notification = {
-				window = { winblend = 0 },
-			},
-		})
-	end,
+	event = "LspAttach",
+	opts = {
+		notification = {
+			window = { winblend = 0 },
+		},
+	},
 }
