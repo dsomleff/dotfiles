6f59d3d95a03b84afeb1be5992f2e0fb4dfe0f1b
Dmytro Somliev <somleff@gmail.com>
2025-12-10 10:18:26 +0100

    Fix neoscroll conflict with harpoon: bad mapping

--- .config/nvim/lua/plugins/neoscroll.lua
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -1,22 +1,24 @@
 return {
 	"karb94/neoscroll.nvim",
-	keys = {
-		{
-			"<C-u>",
-			function()
-				require("neoscroll").ctrl_u({ duration = 400, easing = "sine" })
-			end,
-			desc = "Scroll up smoothly",
-		},
-		{
-			"<C-d>",
-			function()
-				require("neoscroll").ctrl_d({ duration = 400, easing = "sine" })
-			end,
-			desc = "Scroll down smoothly",
-		},
+	event = {
+		"BufReadPost",
 	},
-	opts = {
-		duration_multiplier = 0.3,
-	},
+	opts = function()
+		local neoscroll = require("neoscroll")
+
+		local keymap = {
+			["<C-u>"] = function()
+				neoscroll.ctrl_u({ duration = 400, easing = "sine" })
+			end,
+			["<C-d>"] = function()
+				neoscroll.ctrl_d({ duration = 400, easing = "sine" })
+			end,
+		}
+
+		return {
+			mappings = { "<C-u>", "<C-d>" },
+			duration_multiplier = 0.3,
+			keymap = keymap, -- Assign keymap to config
+		}
+	end,
 }
