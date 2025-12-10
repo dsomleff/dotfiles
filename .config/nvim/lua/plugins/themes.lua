8ca93d551a242a615ee26a992d4411ccfb1a2584
Dmytro Somliev <somleff@gmail.com>
2025-12-10 08:28:09 +0100

    Update themes config: Changed init to config

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -2,7 +2,7 @@ return {
 	"slugbyte/lackluster.nvim",
 	lazy = false,
 	priority = 1000,
-	init = function()
+	config = function()
 		local lackluster = require("lackluster")
 		local color = lackluster.color
 
@@ -18,14 +18,13 @@ return {
 				StatusLineGitDiffAdded = { fg = color.green },
 				StatusLineGitDiffChanged = { fg = color.lack },
 				StatusLineGitDiffRemoved = { fg = color.orange },
-
 				-- LSP diagnostics
 				StatusLineLspError = { fg = color.red, bold = true },
 				StatusLineLspWarn = { fg = color.orange, bold = true },
 				StatusLineLspHints = { fg = color.yellow, bold = true },
 				StatusLineLspInfo = { fg = color.blue, bold = true },
 
-				-- Status line colors (commented in your config originally)
+				-- Status line colors
 				-- StatusLineGit = { bg = "#080808" },
 				-- StatusLineFile = { bg = "#080808" },
 				-- StatusLineCursor = { bg = "#080808" },
@@ -34,8 +33,8 @@ return {
 
 				["@keyword"] = {
 					overwrite = false,
-					-- bold = true,
 					italic = true,
+					-- bold = true,
 				},
 				["@function"] = {
 					overwrite = true,
@@ -53,6 +52,8 @@ return {
 		})
 
 		vim.cmd.colorscheme("lackluster-hack")
+
+		-- Custom highlights
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "FloatBorder", { fg = color.gray9, bg = "#101010" })
