e46929ae7cb0f956b70d5f5dcd2b2958ba4c479f
Dmytro Somliev <somleff@gmail.com>
2025-12-10 08:06:31 +0100

    Update harpoon: rewrite keys part in config

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -2,60 +2,101 @@ return {
 	{
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
 		keys = {
-			vim.keymap.set("n", "<leader>a", function()
-				require("harpoon"):list():add()
-			end, { desc = "Harpoon | Add Mark" }),
-
-			vim.keymap.set("n", "<C-e>", function()
-				require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
-			end, { desc = "Harpoon | Menu" }),
+			{
+				"<leader>a",
+				function()
+					require("harpoon"):list():add()
+				end,
+				desc = "Harpoon | Add Mark",
+			},
+			{
+				"<C-e>",
+				function()
+					require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+				end,
+				desc = "Harpoon | Menu",
+			},
+			{
+				"[w",
+				function()
+					require("harpoon"):list():prev()
+				end,
+				desc = "Harpoon | Previous",
+			},
+			{
+				"]w",
+				function()
+					require("harpoon"):list():next()
+				end,
+				desc = "Harpoon | Next",
+			},
+			{
+				"<leader>1",
+				function()
+					require("harpoon"):list():select(1)
+				end,
+				desc = "Harpoon | File 1",
+			},
+			{
+				"<leader>2",
+				function()
+					require("harpoon"):list():select(2)
+				end,
+				desc = "Harpoon | File 2",
+			},
+			{
+				"<leader>3",
+				function()
+					require("harpoon"):list():select(3)
+				end,
+				desc = "Harpoon | File 3",
+			},
+			{
+				"<leader>4",
+				function()
+					require("harpoon"):list():select(4)
+				end,
+				desc = "Harpoon | File 4",
+			},
+			{
+				"<leader>5",
+				function()
+					require("harpoon"):list():select(5)
+				end,
+				desc = "Harpoon | File 5",
+			},
+			{
+				"<leader>6",
+				function()
+					require("harpoon"):list():select(6)
+				end,
+				desc = "Harpoon | File 6",
+			},
+			{
+				"<leader>7",
+				function()
+					require("harpoon"):list():select(7)
+				end,
+				desc = "Harpoon | File 7",
+			},
+			{
+				"<leader>8",
+				function()
+					require("harpoon"):list():select(8)
+				end,
+				desc = "Harpoon | File 8",
+			},
+			{
+				"<leader>9",
+				function()
+					require("harpoon"):list():select(9)
+				end,
+				desc = "Harpoon | File 9",
+			},
 		},
-
-		vim.keymap.set("n", "[w", function()
-			require("harpoon"):list():prev()
-		end, { desc = "Harpoon | Previous" }),
-
-		vim.keymap.set("n", "]w", function()
-			require("harpoon"):list():next()
-		end, { desc = "Harpoon | Next" }),
-
-		vim.keymap.set("n", "<leader>1", function()
-			require("harpoon"):list():select(1)
-		end),
-
-		vim.keymap.set("n", "<leader>2", function()
-			require("harpoon"):list():select(2)
-		end),
-
-		vim.keymap.set("n", "<leader>3", function()
-			require("harpoon"):list():select(3)
-		end),
-
-		vim.keymap.set("n", "<leader>4", function()
-			require("harpoon"):list():select(4)
-		end),
-
-		vim.keymap.set("n", "<leader>5", function()
-			require("harpoon"):list():select(5)
-		end),
-
-		vim.keymap.set("n", "<leader>6", function()
-			require("harpoon"):list():select(6)
-		end),
-
-		vim.keymap.set("n", "<leader>7", function()
-			require("harpoon"):list():select(7)
-		end),
-
-		vim.keymap.set("n", "<leader>8", function()
-			require("harpoon"):list():select(8)
-		end),
-
-		vim.keymap.set("n", "<leader>9", function()
-			require("harpoon"):list():select(9)
-		end),
 	},
 	{ "nvim-lua/plenary.nvim", lazy = true },
 }
