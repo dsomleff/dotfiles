d04fa9280ff88d4a76f67073b6db25dd797df54a
Dmytro Somliev <somleff@gmail.com>
2026-01-02 21:36:12 +0100

    Synchronize lackluster_theme

--- .config/nvim/lua/fancy_stuff/lackluster_theme.lua
+++ .config/nvim/lua/fancy_stuff/lackluster_theme.lua
@@ -2,11 +2,14 @@ return {
 	"slugbyte/lackluster.nvim",
 	lazy = false,
 	priority = 1000,
-	init = function()
+	config = function()
 		local lackluster = require("lackluster")
 		local color = lackluster.color
 
 		lackluster.setup({
+			tweak_color = {
+				luster = color.gray7,
+			},
 			tweak_ui = {
 				enable_end_of_buffer = true,
 			},
@@ -15,25 +18,48 @@ return {
 				StatusLineGitDiffAdded = { fg = color.green },
 				StatusLineGitDiffChanged = { fg = color.lack },
 				StatusLineGitDiffRemoved = { fg = color.orange },
-
 				-- LSP diagnostics
 				StatusLineLspError = { fg = color.red, bold = true },
 				StatusLineLspWarn = { fg = color.orange, bold = true },
-				StatusLineLspHints = { fg = color.luster, bold = true },
+				StatusLineLspHints = { fg = color.yellow, bold = true },
 				StatusLineLspInfo = { fg = color.blue, bold = true },
 
-				-- Status line colors (commented in your config originally)
+				-- Status line colors
 				-- StatusLineGit = { bg = "#080808" },
 				-- StatusLineFile = { bg = "#080808" },
 				-- StatusLineCursor = { bg = "#080808" },
 				-- StatusLinePercent = { bg = "#080808" },
 				-- StatusLineTotalLines = { bg = "#080808" },
+
+				["@keyword"] = {
+					overwrite = false,
+					italic = true,
+					-- bold = true,
+				},
+				["@function"] = {
+					overwrite = true,
+					link = "@keyword",
+				},
+				["@comment"] = {
+					overwrite = false,
+					italic = true,
+				},
+				["@property"] = {
+					italic = true,
+					overwrite = false,
+				},
 			},
 		})
 
 		vim.cmd.colorscheme("lackluster-hack")
+
+		-- Custom highlights
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "FloatBorder", { fg = color.gray9, bg = "#101010" })
+		vim.api.nvim_set_hl(0, "DiagnosticError", { fg = color.red })
+		vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = color.orange })
+		vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = color.blue })
+		vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = color.yellow })
 	end,
 }
