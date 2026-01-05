5e739b29a795a052204689262614e17c02839868
Dmytro Somliev <somleff@gmail.com>
2026-01-05 17:12:45 +0100

    treesitter master to main changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,36 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
+	branch = "main",
 	event = { "BufReadPre", "BufNewFile" },
-	-- lazy = false, // remove events and use this in case of wrong behavior
 	build = ":TSUpdate",
 
 	config = function()
-		local config = require("nvim-treesitter.configs")
-
-		config.setup({
-			autotag = {
-				enable = true,
-			},
-
-			ensure_installed = { "vimdoc", "javascript", "typescript", "lua", "go", "jsdoc", "vim" },
-
-			sync_install = false,
-
-			auto_install = true,
-
+		require("nvim-treesitter").setup({
 			indent = {
 				enable = true,
 			},
 
 			highlight = {
 				enable = true,
-
 				additional_vim_regex_highlighting = { "markdown" },
 			},
+
+			autotag = {
+				enable = true,
+			},
 		})
+
+		-- To install a new treesitter run TSInstall and name of the lang
+		-- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
+		-- "vimdoc",
+		-- "javascript",
+		-- "typescript",
+		-- "lua",
+		-- "go",
+		-- "jsdoc",
+		-- "vim",
+		-- "toml"
+		-- "dockerfile"
+		-- "bash"
 	end,
 }
