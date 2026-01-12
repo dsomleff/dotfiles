43113568a225aeeeb9790f51cda06449172e5911
Dmytro Somliev <somleff@gmail.com>
2026-01-12 15:21:35 +0100

    Add nvim-treesitter (main branch) into nvim-new

--- .config/nvim-new/lua/plugins.lua
+++ .config/nvim-new/lua/plugins.lua
@@ -3,12 +3,20 @@ vim.pack.add({
     { src = "https://github.com/tpope/vim-fugitive" },
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
     { src = "https://github.com/stevearc/oil.nvim" },
+    {
+        src = "https://github.com/nvim-treesitter/nvim-treesitter",
+        version = "main",
+        data = {
+          on_update = function()
+            vim.cmd('TSUpdate')
+          end,
+        },
+    },
 })
 
 -------------------------------------------------
--- 2️⃣Configure them
+-- Theme
 -------------------------------------------------
--- vim.cmd(":hi statusline guibg=NONE")
 local lackluster = require("lackluster")
 local color = lackluster.color
 
@@ -18,14 +26,14 @@ lackluster.setup({
   },
   tweak_highlight = {
         -- Git diff
-    StatusLineGitDiffAdded = { fg = color.green },
-    StatusLineGitDiffChanged = { fg = color.lack },
-    StatusLineGitDiffRemoved = { fg = color.orange },
+    -- StatusLineGitDiffAdded = { fg = color.green },
+    -- StatusLineGitDiffChanged = { fg = color.lack },
+    -- StatusLineGitDiffRemoved = { fg = color.orange },
     -- LSP diagnostics
-    StatusLineLspError = { fg = color.red, bold = true },
-    StatusLineLspWarn = { fg = color.orange, bold = true },
-    StatusLineLspHints = { fg = color.yellow, bold = true },
-    StatusLineLspInfo = { fg = color.blue, bold = true },
+    -- StatusLineLspError = { fg = color.red, bold = true },
+    -- StatusLineLspWarn = { fg = color.orange, bold = true },
+    -- StatusLineLspHints = { fg = color.yellow, bold = true },
+    -- StatusLineLspInfo = { fg = color.blue, bold = true },
 
     ["@keyword"] = {
       overwrite = false,
@@ -47,10 +55,11 @@ lackluster.setup({
 })
 
 vim.cmd("colorscheme lackluster")
+-- vim.cmd(":hi statusline guibg=NONE")
 
 -- Custom highlights
-vim.api.nvim_set_hl(0, "Normal",        { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat",   { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal",        { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat",   { bg = "none" })
 -- vim.api.nvim_set_hl(0, "FloatBorder",   { fg = color.gray9, bg = "#101010" })
 
 -- Override Gitsigns highlights
@@ -58,6 +67,9 @@ vim.api.nvim_set_hl(0, "NormalFloat",   { bg = "none" })
 -- vim.api.nvim_set_hl(0, "GitSignsChange", { fg = color.lack,   bg = "none" })
 -- vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = color.orange, bg = "none" })
 
+-------------------------------------------------
+-- Oil
+-------------------------------------------------
 require "oil".setup({
     -- default_file_explorer = false,
     delete_to_trash = true,
@@ -65,3 +77,68 @@ require "oil".setup({
         show_hidden = true,
     },
 })
+-------------------------------------------------
+-- Treesitter
+-------------------------------------------------
+local parsers = {
+  "bash",
+  "dockerfile",
+  "git_config",
+  "git_rebase",
+  "gitattributes",
+  "gitcommit",
+  "gitignore",
+  "go",
+  "html",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "query",
+  "toml",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
+}
+require("nvim-treesitter.config").setup({
+    ensure_installed = parsers,
+    auto_install = true,
+    highlight = {
+        enable = true,
+    },
+    indent = {
+        enable = true,
+    },
+})
+
+local function packchanged(event)
+  local after = event.data.spec.data and event.data.spec.data.after
+  if not after then return end
+
+  local pkg_name = event.data.spec.name
+
+  local retries = 0
+  local function wait()
+    if retries > 20 then return end  -- avoid infinite loop
+    retries = retries + 1
+
+    package.loaded[pkg_name] = nil
+    local ok, _ = pcall(require, pkg_name)
+
+    if ok then
+      if type(after) == "function" then
+        after()
+      elseif type(after) == "string" then
+        pcall(vim.cmd, after)
+      end
+    else
+      vim.defer_fn(wait, 50)
+    end
+  end
+
+  wait()
+end
+
+vim.api.nvim_create_autocmd("packchanged", { callback = packchanged })
