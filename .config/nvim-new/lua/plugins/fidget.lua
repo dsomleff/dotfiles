1104db5c9e1023a89742939eeba852d02a6f82ba
Dmytro Somliev <somleff@gmail.com>
2026-01-13 15:10:37 +0100

    Add fidget into nvim-new

---
+++ .config/nvim-new/lua/plugins/fidget.lua
@@ -0,0 +1,16 @@
+vim.pack.add({
+    { src = "https://github.com/j-hui/fidget.nvim" }
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	once = true,
+	callback = function()
+		require("fidget").setup({
+			notification = {
+				window = {
+					winblend = 0,
+				},
+			},
+		})
+	end,
+})
