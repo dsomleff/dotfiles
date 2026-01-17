df709d34d9b1e2154c29afd42e57f555bd1563a7
Dmytro Somliev <somleff@gmail.com>
2026-01-18 00:19:57 +0100

    nvim-new: remove "lazy loading" for blink

--- .config/nvim-new/lua/plugins/blink.lua
+++ .config/nvim-new/lua/plugins/blink.lua
@@ -6,57 +6,43 @@ vim.pack.add({
 	"https://github.com/rafamadriz/friendly-snippets",
 })
 
-local blink_loaded = false
-
-local function load_blink()
-	if blink_loaded then
-		return
-	end
-	blink_loaded = true
-
-	require("blink.cmp").setup({
-		keymap = {
-			preset = "super-tab",
-		},
-		appearance = {
-			nerd_font_variant = "mono",
-		},
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-			providers = {
-				lsp = {
-					name = "LSP",
-					module = "blink.cmp.sources.lsp",
-					transform_items = function(_, items)
-						local kinds = require("blink.cmp.types").CompletionItemKind
-						return vim.tbl_filter(function(item)
-							return item.kind ~= kinds.Keyword
-						end, items)
-					end,
-				},
+require("blink.cmp").setup({
+	keymap = {
+		preset = "super-tab",
+	},
+	appearance = {
+		nerd_font_variant = "mono",
+	},
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+		providers = {
+			lsp = {
+				name = "LSP",
+				module = "blink.cmp.sources.lsp",
+				transform_items = function(_, items)
+					local kinds = require("blink.cmp.types").CompletionItemKind
+					return vim.tbl_filter(function(item)
+						return item.kind ~= kinds.Keyword
+					end, items)
+				end,
 			},
 		},
-		signature = {
-			enabled = true,
+	},
+	signature = {
+		enabled = true,
+	},
+	completion = {
+		menu = {
+			border = "single",
 		},
-		completion = {
-			menu = {
+		documentation = {
+			auto_show = true,
+			window = {
 				border = "single",
 			},
-			documentation = {
-				auto_show = true,
-				window = {
-					border = "single",
-				},
-			},
 		},
-		fuzzy = {
-			implementation = "prefer_rust_with_warning",
-		},
-	})
-end
-
-vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
-	once = true,
-	callback = load_blink,
+	},
+	fuzzy = {
+		implementation = "prefer_rust_with_warning",
+	},
 })
