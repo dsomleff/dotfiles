820f911df01f477f4347b91fd8f0390ee2855a33
Dmytro Somliev <somleff@gmail.com>
2026-01-18 09:48:41 +0100

    nvim-new: treesitter fix config (for master branch)

--- .config/nvim-new/lua/plugins/treesitter.lua
+++ .config/nvim-new/lua/plugins/treesitter.lua
@@ -1,12 +1,7 @@
 vim.pack.add({
 	{
 		src = "https://github.com/nvim-treesitter/nvim-treesitter",
-		version = "main",
-		data = {
-			on_update = function()
-				vim.cmd("TSUpdate")
-			end,
-		},
+		version = "master",
 	},
 })
 
@@ -22,17 +17,12 @@ local parsers = {
 	"html",
 	"javascript",
 	"json",
-	"lua",
-	"markdown",
-	"query",
 	"toml",
 	"tsx",
 	"typescript",
-	"vim",
-	"vimdoc",
 	"yaml",
 }
-require("nvim-treesitter.config").setup({
+require("nvim-treesitter.configs").setup({
 	ensure_installed = parsers,
 	auto_install = true,
 	highlight = {
@@ -42,37 +32,3 @@ require("nvim-treesitter.config").setup({
 		enable = true,
 	},
 })
-
-local function packchanged(event)
-	local after = event.data.spec.data and event.data.spec.data.after
-	if not after then
-		return
-	end
-
-	local pkg_name = event.data.spec.name
-
-	local retries = 0
-	local function wait()
-		if retries > 20 then
-			return
-		end
-		retries = retries + 1
-
-		package.loaded[pkg_name] = nil
-		local ok, _ = pcall(require, pkg_name)
-
-		if ok then
-			if type(after) == "function" then
-				after()
-			elseif type(after) == "string" then
-				pcall(vim.cmd, after)
-			end
-		else
-			vim.defer_fn(wait, 50)
-		end
-	end
-
-	wait()
-end
-
-vim.api.nvim_create_autocmd("packchanged", { callback = packchanged })
