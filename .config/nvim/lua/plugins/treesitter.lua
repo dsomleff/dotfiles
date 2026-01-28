2151c0f7c43ac4366871a07375f8646643c6f7c8
Dmytro Somliev <somleff@gmail.com>
2026-01-28 13:33:21 +0100

    nvim: treesitter update

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -60,13 +60,11 @@ return {
 
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = filetypes,
+
 			callback = function()
-				-- syntax highlighting, provided by Neovim
 				vim.treesitter.start()
-				-- folds, provided by Neovim
 				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 				vim.wo.foldmethod = "expr"
-				-- indentation, provided by nvim-treesitter
 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 			end,
 		})
